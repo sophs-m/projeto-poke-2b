256b2b113ef8eef50306ece9c8a8822f66b833d1
sophs-m <rm94485@estudante.fieb.edu.br>
2025-05-05 12:28:38 -0300

    pokemonimg

---
+++ src/Home.jsx
@@ -0,0 +1,16 @@
+import poke from "./assets/pokeapi_256.png"
+
+const Home = ()=> {
+    return (
+        <div className="pokeflex">
+            <img src={poke} />
+            <img src={poke} />
+            <img src={poke} />
+            <img src={poke} />
+            <img src={poke} />
+            <img src={poke} />
+        </div>
+    )
+}
+
+export default Home
